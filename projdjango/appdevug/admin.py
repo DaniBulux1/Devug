60004e35755aa34fda4b4695fa4896eb77818be1
Isaac7uiz <160290684+Isaac7uiz@users.noreply.github.com>
2025-05-31 10:39:30 -0600

    Configuración de modelos en admin.py

--- projdjango/appdevug/admin.py
+++ projdjango/appdevug/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Tutorial
+from .models import Reaccion
+# Register your models here.
+admin.site.register(Tutorial)
+admin.site.register(Reaccion)
