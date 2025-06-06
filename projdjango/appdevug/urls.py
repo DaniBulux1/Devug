821ea07fecc05ce33e4046f97d393ea759b4d481
KevenBM7 <kevinbixcul@gmail.com>
2025-06-05 23:34:08 -0600

    se gregaron las rutas API para sistema de reacciones

--- projdjango/appdevug/urls.py
+++ projdjango/appdevug/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Rutas principales
     path('', views.lista_tutoriales, name='lista_tutoriales'),
     path('tutorial/<int:pk>/', views.detalle_tutorial, name='detalle_tutorial'),
     path('publicar/', views.crear_tutorial, name='crear_tutorial'),
-    # Nueva ruta para editar tutoriales
     path('editar/<int:pk>/', views.editar_tutorial, name='editar_tutorial'),
+    
+    # APIs para reacciones
+    path('api/reaccion/', views.agregar_reaccion, name='agregar_reaccion'),
+    path('api/reacciones/<int:tutorial_id>/', views.obtener_reacciones, name='obtener_reacciones'),
 ]
